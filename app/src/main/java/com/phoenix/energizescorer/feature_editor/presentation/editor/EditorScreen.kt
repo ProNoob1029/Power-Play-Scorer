cb13b54189d7f52bf54f96eed692f762cb809fc7
ProNoob1029 <pronoob1029@gmail.com>
2022-09-07 16:59:32 +0300

    working on recomposition

--- app/src/main/java/com/phoenix/energizescorer/feature_editor/presentation/editor/EditorScreen.kt
+++ app/src/main/java/com/phoenix/energizescorer/feature_editor/presentation/editor/EditorScreen.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -16,6 +13,7 @@ import androidx.navigation.compose.rememberNavController
 import com.phoenix.energizescorer.feature_editor.domain.model.Match
 import com.phoenix.energizescorer.feature_editor.presentation.editor.components.AllianceButtons
 import com.phoenix.energizescorer.feature_editor.presentation.editor.components.TextField
+import com.phoenix.energizescorer.feature_editor.presentation.editor.components.Title
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
@@ -28,7 +26,7 @@ fun EditorScreen(
     val state = viewModel.state.collectAsState()
     val screenList = remember {
         screenList(
-            state = state,
+            _state = state,
             mutableState = viewModel.state
         )
     }
@@ -47,15 +45,37 @@ fun EditorScreen(
 }
 
 fun screenList(
-    state: State<Match>,
+    _state: State<Match>,
     mutableState: MutableStateFlow<Match>
 ): List<@Composable (modifier: Modifier) -> Unit> {
+    val state by _state
+    val autoPoints = derivedStateOf {
+        _state.value.let {
+            it.autoDuck * 10 +
+                    it.autoFreightBonus1 * 10 +
+                    it.autoFreightBonus2 * 10 * it.twoTeams +
+                    it.autoStorage * 2 +
+                    (it.autoHub1 + it.autoHub2 + it.autoHub3) * 6 +
+                    when(it.autoParked1) {
+                        false -> 3
+                        true -> 5
+                        else -> 0
+                    } * (it.autoFullyParked1 + 1) +
+                    when(it.autoParked2) {
+                        false -> 3
+                        true -> 5
+                        else -> 0
+                    } * (it.autoFullyParked2 + 1) * it.twoTeams +
+                    it.alliance * 2
+        }
+    }
+
     return listOf(
         { modifier ->
             TextField(
                 modifier = modifier,
                 label = "Title",
-                text = state.value.title,
+                text = state.title,
                 onValueChange = { newString ->
                     mutableState.update {
                         it.copy(
@@ -71,7 +91,7 @@ fun screenList(
                 modifier = modifier,
                 firstText = "Red Alliance",
                 secondText = "Blue Alliance",
-                activeIndex = state.value.alliance,
+                activeIndex = state.alliance,
                 onButtonClicked =
                 { index ->
                     mutableState.update { match ->
@@ -82,6 +102,21 @@ fun screenList(
                 },
                 enabled = true
             )
+        },
+        { modifier ->
+            Title(
+                modifier = modifier,
+                title = "Autonomous points: ",
+                counter = autoPoints.value
+            )
         }
     )
 }
+
+private operator fun Boolean.plus(other: Int) = if (this) other + 1 else other
+private operator fun Boolean.times(other: Int) = if (this) other else 0
+private operator fun Int.times(other: Boolean) = if (other) this else 0
+
+private operator fun Boolean?.plus(other: Int) = if (this == null) 0 else this + 1 + other
+private operator fun Boolean?.times(other: Int) = if (this == null) 0 else (this + 1) * other
+private operator fun Int.times(other: Boolean?) = if (other == null) 0 else (other + 1) * this
---
+++ app/src/main/java/com/phoenix/energizescorer/feature_editor/presentation/editor/components/Title.kt
@@ -0,0 +1,71 @@
+package com.phoenix.energizescorer.feature_editor.presentation.editor.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.phoenix.energizescorer.feature_editor.presentation.util.MeasureView
+
+@Composable
+fun Title (
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues(top = 8.dp),
+    title: String,
+    counter: Int,
+    textStyle: TextStyle = MaterialTheme.typography.headlineMedium,
+    surfaceColor: Color = MaterialTheme.colorScheme.primaryContainer,
+) {
+
+    Surface (
+        modifier = modifier
+            .padding(paddingValues)
+            .fillMaxWidth(),
+        color = surfaceColor
+    ) {
+        MeasureView(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            viewToMeasure = {
+                Text(
+                    text = "$title$counter",
+                    style = textStyle
+                )
+            }
+        ) { maxWidth, measuredWidth, _ ->
+            val compact = maxWidth < measuredWidth
+            if (compact) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "$title$counter",
+                    style = textStyle.copy(fontFeatureSettings = "tnum"),
+                    textAlign = TextAlign.Center
+                )
+            } else {
+                Row(
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = title,
+                        style = textStyle,
+                        textAlign = TextAlign.Start
+                    )
+                    Text(
+                        text = "$counter",
+                        style = textStyle.copy(fontFeatureSettings = "tnum"),
+                        textAlign = TextAlign.End
+                    )
+                }
+            }
+        }
+    }
+}
