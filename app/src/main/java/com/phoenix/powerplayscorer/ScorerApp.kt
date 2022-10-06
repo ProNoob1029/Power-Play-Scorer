d5227eb9d183a982304fa20f82dd3f6cfcfceefe
ProNoob1029 <pronoob1029@gmail.com>
2022-10-06 22:11:15 +0300

    clearing persistence on start

--- app/src/main/java/com/phoenix/powerplayscorer/ScorerApp.kt
+++ app/src/main/java/com/phoenix/powerplayscorer/ScorerApp.kt
@@ -1,8 +1,18 @@
 package com.phoenix.powerplayscorer
 
 import android.app.Application
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class ScorerApp @Inject constructor(): Application()
+class ScorerApp @Inject constructor(): Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.firestore.clearPersistence()
+        Log.e(TAG, "Persistence cleared")
+    }
+}
