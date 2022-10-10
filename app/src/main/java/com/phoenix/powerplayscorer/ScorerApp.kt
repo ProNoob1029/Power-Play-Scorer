e1aa16d98fe5190f37bd2d88ca7cf6d5643f638f
ProNoob1029 <pronoob1029@gmail.com>
2022-10-10 21:43:50 +0300

    added play integrity

--- app/src/main/java/com/phoenix/powerplayscorer/ScorerApp.kt
+++ app/src/main/java/com/phoenix/powerplayscorer/ScorerApp.kt
@@ -3,6 +3,9 @@ package com.phoenix.powerplayscorer
 import android.app.Application
 import android.content.ContentValues.TAG
 import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
@@ -12,6 +15,11 @@ import javax.inject.Inject
 class ScorerApp @Inject constructor(): Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(/*context=*/this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance()
+        )
         Firebase.firestore.clearPersistence()
         Log.e(TAG, "Persistence cleared")
     }
